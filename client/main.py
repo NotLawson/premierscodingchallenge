84d97f0d3e09c1fceafc375f229c8eb7cac34df3
Lawson C <lawson.conallin@gmail.com>
2024-05-13 11:45:35 +1000

    learning how to tkinter

--- client/main.py
+++ client/main.py
@@ -3,10 +3,31 @@
 ##
 ## The app will be tkinter app with that can take wikipedia results 
 
-
-import tkinter as tk
+from tkinter import Tk, Label, Button, Entry
 #import tensorflow as tf
 
 from wikipedia_bypass import wiki
 
-wiki("google")
+tk = Tk(screenName="Flashcards")
+
+tk.title("Flashcards")
+
+# Widget Functions
+def submit():
+    message = widgets["input"].get()
+    widgets["output"].configure(text=message)
+    pass
+
+
+
+## Widgets
+widgets = {
+    "output":Label(tk, text="Hi connor", ),
+    "input":Entry(tk),
+    "submit":Button(tk, text="Submit", command = submit),
+
+}
+for widget in widgets.keys():
+    widgets[widget].pack()
+    
+tk.mainloop()
